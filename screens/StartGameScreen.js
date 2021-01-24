04ca2926cd0addcf28652297d012f890405bb66b
Chris <cwill833@gmail.com>
2021-01-23 22:19:56 -0500

    create a number container to hold confirmed number, adjust start game screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native'
 import Card from '../components/Card'
 import Colors from '../constants/colors'
 import Input from '../components/Input'
+import NumberContainer from '../components/NumberContainer'
 const StartGameScreen = props => {
 
   const [enteredValue, setEnteredValue] = useState('')
@@ -20,23 +21,33 @@ const StartGameScreen = props => {
 
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue)
-    if(isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99){
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert('Invalid number!', 'Number has to be a number between 1 and 99', [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }])
       return
     }
     setConfirmed(true)
     setSelectedNumber(chosenNumber)
     setEnteredValue('')
+    Keyboard.dismiss()
   }
 
   let confirmedOutput
 
-  if(confirmed){
-    confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
+  if (confirmed) {
+    confirmedOutput = (
+      <Card style={styles.summaryContainer}>
+        <Text>You selected</Text>
+        <NumberContainer>
+          {selectedNumber}
+        </NumberContainer>
+        <Button title="START GAME" />
+      </Card> 
+    )
   }
 
   return (
     //this is needed in order to click on the screen and dismiss the keyboard if one is up
-    <TouchableWithoutFeedback onPress={()=> {Keyboard.dismiss()}}>
+    <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss() }}>
       <View style={styles.screen}>
         <Text style={styles.title}>Start a New Game!</Text>
         <Card style={styles.inputContainer}>
@@ -80,6 +91,10 @@ const styles = StyleSheet.create({
   input: {
     width: 50,
     textAlign: 'center'
+  },
+  summaryContainer: {
+    marginTop: 20,
+    alignItems: 'center'
   }
 })
 
