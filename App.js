bc879bf946f22148407938e5ff4d9f1cbb84a094
Chris <cwill833@gmail.com>
2021-01-24 15:03:06 -0500

    adding fonts and how to load them async

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font'
+// this is the new version for react native if needing to use AppLoading expo install expo-app-loading
+import AppLoading from 'expo-app-loading'
 import Header from './components/Header'
 import GameOverScreen from './screens/GameOverScreen';
 import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
 
+const fetchDFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
+
 export default function App() {
 
   const [userNumber, setUserNumber] = useState()
   const [guessRounds, setGuessRounds] = useState(0)
+  const [dataLoaded, setDataLoaded] = useState(false)
+
+  // this is how we can check and use AppLoading to make sure 
+  // all actions that need to resolve before app runs do
+  if (!dataLoaded) {
+    return <AppLoading startAsync={fetchDFonts} onFinish={() => setDataLoaded(true)} onError={(err) => { console.log(err) }} />
+  }
 
   const newGameHandler = () => {
     setGuessRounds(0)
@@ -24,12 +41,12 @@ export default function App() {
     setGuessRounds(numOfRounds)
   }
 
-  let content = <StartGameScreen onStartGame={startGameHandler}/>
+  let content = <StartGameScreen onStartGame={startGameHandler} />
 
-  if(userNumber && guessRounds <= 0){
-    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>
-  } else if(guessRounds > 0){
-    content = <GameOverScreen onRestart={newGameHandler} roundsNumber={guessRounds} userNumber={userNumber}/>
+  if (userNumber && guessRounds <= 0) {
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+  } else if (guessRounds > 0) {
+    content = <GameOverScreen onRestart={newGameHandler} roundsNumber={guessRounds} userNumber={userNumber} />
   }
   return (
     <View style={styles.screen}>
