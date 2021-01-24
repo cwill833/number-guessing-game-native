e669d4986a1d566d878e1a17c9a61839eeb51ce9
Chris <cwill833@gmail.com>
2021-01-23 21:25:48 -0500

    add keyboard validation and interact with keyboard api by dismissing keyboard once screen is clicked

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,22 +1,31 @@
-import React from 'react'
-import {View, Text, StyleSheet, TextInput, Button} from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import Card from '../components/Card'
 import Colors from '../constants/colors'
 import Input from '../components/Input'
 const StartGameScreen = props => {
 
-  return(
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start a New Game!</Text>
-      <Card style={styles.inputContainer}>
-        <Text>Select a Number</Text>
-        <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType='numeric' maxLength={2}/>
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}><Button color={Colors.primary} title="Reset"onPress={()=>{}}/></View>
-          <View style={styles.button}><Button color={Colors.accent} title="Confirm" onPress={()=>{}}/></View>
-        </View>
-      </Card>
-    </View>
+  const [enteredValue, setEnteredValue] = useState('')
+
+  const numberInputHandler = textInput => {
+    // this is how we validate and replace and value that is not a number between 0-9
+    setEnteredValue(textInput.replace(/[^0-9]/g, ''))
+  }
+  return (
+    //this is needed in order to click on the screen and dismiss the keyboard if one is up
+    <TouchableWithoutFeedback onPress={()=> {Keyboard.dismiss()}}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a New Game!</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a Number</Text>
+          <Input value={enteredValue} onChangeText={numberInputHandler} style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType='number-pad' maxLength={2} />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}><Button color={Colors.primary} title="Reset" onPress={() => { }} /></View>
+            <View style={styles.button}><Button color={Colors.accent} title="Confirm" onPress={() => { }} /></View>
+          </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
